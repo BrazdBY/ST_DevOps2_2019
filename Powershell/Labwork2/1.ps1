dbf9b9b55a318855924df75c125c93c5ed095d34
Maksim Pahuliayeu <brazd.by@gmail.com>
2019-02-13 02:17:00 +0300

    Labwork3

--- Powershell/Labwork2/1.ps1
+++ Powershell/Labwork2/1.ps1
@@ -1,7 +1,14 @@
-﻿#1.	Просмотреть содержимое ветви реeстра HKCU
-Get-PSDrive HKCU 
-cd hkCU:\
-Get-ChildItem . | ft - # выводим содержимое корня древа
+﻿[CmdletBinding()]
+Param (
+  [parameter(Mandatory=$false, HelpMessage="Enter name of REG Branche")]
+  [string]$Reg_branche = "HKCU"
+
+  
+)
+#1.	Просмотреть содержимое ветви реeстра HKCU
+Get-PSDrive $Reg_branche 
+
+Get-ChildItem $Reg_branche + "." | ft - # выводим содержимое корня древа
 
 Get-ChildItem . -Recurse -ErrorAction SilentlyContinue # выводим рекурсивно все записи древа. При ограниченной учетной записи можно воспользоваться ключом  -ErrorAction SilentlyContinue 
 
---
+++ Powershell/Labwork3/Get-Reg-Branche.ps1
@@ -0,0 +1,46 @@
+﻿
+[CmdletBinding()]
+<#
+    .SYNOPSIS
+        Вывод содержимого ветви реестра в консоль.
+    .DESCRIPTION
+        Данная функция выводит одержимое одной из ветвей реестра в консоль PowerShell
+    .EXAMPLE
+        #Get-Reg-Branche -REG HKLM - Данная команда выведет в консоль ветвь реестра HKLM 
+    
+    .PARAMETER REG
+        Ветвь реестра, которую необходимо вывести (обязательный параметр)
+    
+    #>
+Param (
+  [parameter(   Mandatory=$true,
+                Position=1,
+                ValueFromPipeline=$true,
+                ParameterSetName="REG",
+                HelpMessage="Enter name of REG Branche")]
+  
+                #[ValidateScript({($_ -eq "HKLM1") -or ($_ -eq "HKCU2") })]
+                #[ValidateSet("HKLM3", "HKCU4")]
+                [ValidateNotNull()]
+                [string]$Reg_branche,
+
+                [parameter(   Mandatory=$true,
+                Position=1,
+                ValueFromPipeline=$true,
+                ParameterSetName="REG",
+                HelpMessage="Enter name of REG Branche")]
+                [string]$temer="true"             
+  
+)
+
+#1.	Просмотреть содержимое ветви реeстра HKCU
+Get-PSDrive $Reg_branche 
+
+#Get-ChildItem . | ft - # выводим содержимое корня древа
+
+Get-ChildItem ($Reg_branche + ":\") -Recurse -ErrorAction SilentlyContinue # выводим рекурсивно все записи древа. При ограниченной учетной записи можно воспользоваться ключом  -ErrorAction SilentlyContinue 
+
+
+
+
+
