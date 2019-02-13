f6fcb681fd10b8f681afd6ccab95d960f09b3761
Maksim Pahuliayeu <brazd.by@gmail.com>
2019-02-13 23:07:06 +0300

    merge

--- Powershell/Labwork2/1.ps1
+++ Powershell/Labwork2/1.ps1
@@ -1,4 +1,4 @@
-﻿[CmdletBinding()]
+[CmdletBinding()]
 Param (
   [parameter(Mandatory=$false, HelpMessage="Enter name of REG Branche")]
   [string]$Reg_branche = "HKCU"
@@ -10,7 +10,7 @@ Get-PSDrive $Reg_branche
 
 Get-ChildItem $Reg_branche + "." | ft - # выводим содержимое корня древа
 
-Get-ChildItem . -Recurse -ErrorAction SilentlyContinue # выводим рекурсивно все записи древа. При ограниченной учетной записи можно воспользоваться ключом  -ErrorAction SilentlyContinue 
+Get-ChildItem . -Recurse -ErrorAction SilentlyContinue | Select-Object -Property Name # выводим рекурсивно все записи древа. При ограниченной учетной записи можно воспользоваться ключом  -ErrorAction SilentlyContinue 
 
 
 
